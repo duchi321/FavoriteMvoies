ab6c4da515a0f4b1a2d15c551242bac59be4099f
duchi321 <sqaz0502@gmail.com>
2024-03-28 18:27:38 +0800

    renderMoviesList X 80 item.

--- favorite.js
+++ favorite.js
@@ -2,11 +2,46 @@ const BASE_URL = 'https://webdev.alphacamp.io'
 const INDEX_URL = BASE_URL + '/api/movies/'
 const POSTER_URL = BASE_URL + '/posters/'
 const movies = []
+const dataPanel = document.querySelector('#data-panel')
 
 axios
     .get(INDEX_URL)
     .then((response) => {
         movies.push(...response.data.results)
-        console.log(movies)
+        renderMoviesList(movies)
     })
     .catch((err) => console.log(err))
+
+
+function renderMoviesList(data) {
+    let htmlContent = ''
+    data.forEach(item => {
+        htmlContent += `
+              <div class="col-sm-3">
+    <div class="mb-2">
+      <div class="card">
+        <img
+          src="${POSTER_URL + item.image}"
+          class="card-img-top"
+          alt="Movie Poster"
+        />
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+        </div>
+        <div class="card-footer">
+          <button
+            class="btn btn-primary btn-show-movie"
+            data-bs-toggle="modal"
+            data-bs-target="#movie-modal"
+          >
+            More
+          </button>
+          <button class="btn btn-success btn-add-favorite">+</button>
+        </div>
+      </div>
+    </div>
+  </div>
+        `
+    })
+    dataPanel.innerHTML = htmlContent
+}
