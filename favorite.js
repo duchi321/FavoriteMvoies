edf0cb4f58e26cd0db74f1c47b811bb659e65512
duchi321 <sqaz0502@gmail.com>
2024-03-30 16:54:17 +0800

    add showMovieModel & Optimize code

--- favorite.js
+++ favorite.js
@@ -5,18 +5,18 @@ const movies = []
 const dataPanel = document.querySelector('#data-panel')
 
 axios
-    .get(INDEX_URL)
-    .then((response) => {
-        movies.push(...response.data.results)
-        renderMoviesList(movies)
-    })
-    .catch((err) => console.log(err))
+  .get(INDEX_URL)
+  .then(response => {
+    movies.push(...response.data.results)
+    renderMoviesList(movies)
+  })
+  .catch(err => console.log(err))
 
 
 function renderMoviesList(data) {
-    let htmlContent = ''
-    data.forEach(item => {
-        htmlContent += `
+  let htmlContent = ''
+  data.forEach(item => {
+    htmlContent += `
               <div class="col-sm-3">
     <div class="mb-2">
       <div class="card">
@@ -33,6 +33,7 @@ function renderMoviesList(data) {
             class="btn btn-primary btn-show-movie"
             data-bs-toggle="modal"
             data-bs-target="#movie-modal"
+            data-id=${item.id}
           >
             More
           </button>
@@ -42,6 +43,31 @@ function renderMoviesList(data) {
     </div>
   </div>
         `
-    })
-    dataPanel.innerHTML = htmlContent
+  })
+  dataPanel.innerHTML = htmlContent
 }
+
+function showMovieModel(id) {
+  const modelTitle = document.querySelector('#movie-modal-title')
+  const modelImg = document.querySelector('#movie-modal-image')
+  const modelDate = document.querySelector('#movie-modal-date')
+  const modelDescription = document.querySelector('#movie-modal-description')
+  axios.get(INDEX_URL + id).then(response => {
+    const data = response.data.results
+    modelTitle.textContent = data.title
+    modelImg.innerHTML = `<img src="${POSTER_URL + data.image}" alt="movie-poster" class="img-fluid">`
+    modelDate.textContent = `Release date: ${data.release_date}`
+    modelDescription.textContent = data.description
+  }).catch(err => {
+    console.log(err)
+  })
+}
+
+dataPanel.addEventListener('click', function onPanelClicked(e) {
+  if (e.target.matches('.btn-show-movie')) {
+    console.log(e.target)
+    console.log(e.target.dataset.id)
+    showMovieModel(e.target.dataset.id)
+  }
+})
+
